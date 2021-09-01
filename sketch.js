db3ff9ee75dc5ac919f96406989d6ab8d490573b
shaurayCoder00001 <88592990+shaurayCoder00001@users.noreply.github.com>
2021-09-01 12:46:29 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -11,7 +11,7 @@ var fruits;
 var fruitGroup;
 var fruit1_img, fruit2_img, fruit3_img, fruit4_img, fruit5_img;
 var player_img;
-
+var game 
 
 function preload(){
   back_img = loadImage("images/jungle.jpg");
